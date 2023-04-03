917aa138f6f9e78e78d2cf9c57ab07377e80fcf7
Kouta Nakano <knakano95@gmail.com>
2023-04-03 23:24:34 +0900

    fix css

--- components/molecules/LoginForm/styles.ts
+++ components/molecules/LoginForm/styles.ts
@@ -4,12 +4,15 @@ import SubmitButton from '~/components/atoms/SubmitButton/SubmitButton.vue'
 import FormVue from '~/components/atoms/FormVue/FormVue.vue'
 
 export const Form = styled(FormVue)`
+  position: absolute;
+  top: calc(50% - 220px);
+  left: 0;
+  right: 0;
   display: block;
   max-width: 420px;
   margin: auto;
   padding: 10px;
   box-sizing: border-box;
-  margin-top: calc(50% - 440px);
 `
 
 export const PasswordInput = styled(TextInput)`
--- components/templates/LoginLayout/styles.ts
+++ components/templates/LoginLayout/styles.ts
@@ -1,6 +1,7 @@
 import styled from 'vue-styled-components'
 
 export const Container = styled.div`
+  position: relative;
   height: 100vh;
   width: 100vw;
   padding: 10px;
